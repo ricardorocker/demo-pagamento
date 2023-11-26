f35f5b6ada849c2fe14c9543c8634c27cf31afb8
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-11-26 17:08:48 -0300

    action buttons

--- src/app/views/demo-pagamento-table/demo-pagamento-table.component.ts
+++ src/app/views/demo-pagamento-table/demo-pagamento-table.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-demo-pagamento-table',
   templateUrl: './demo-pagamento-table.component.html',
-  styleUrls: ['./demo-pagamento-table.component.css']
+  styleUrls: ['./demo-pagamento-table.component.css'],
 })
 export class DemoPagamentoTableComponent {
-
+  isEdit: boolean = true;
 }
