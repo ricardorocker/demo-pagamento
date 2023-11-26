12218e0b440064f1c13b8c374e1f7c33577b83b7
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-11-26 16:26:33 -0300

    profile-card-component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DemoPagamentoComponent } from './views/demo-pagamento/demo-pagamento.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DemoPagamentoComponent
+    DemoPagamentoComponent,
+    ProfileCardComponent
   ],
   imports: [
     BrowserModule,
