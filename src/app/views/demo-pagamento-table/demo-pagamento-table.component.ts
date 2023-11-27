587ec15d4bd50211acee759bb79e9b0e39d5c28d
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-11-27 09:13:10 -0300

    @input to handle with isEdit

--- src/app/views/demo-pagamento-table/demo-pagamento-table.component.ts
+++ src/app/views/demo-pagamento-table/demo-pagamento-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-demo-pagamento-table',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo-pagamento-table.component.css'],
 })
 export class DemoPagamentoTableComponent {
-  isEdit: boolean = true;
+  @Input() isEdit: boolean = false;
 }
--- src/app/views/demo-pagamento/demo-pagamento.component.ts
+++ src/app/views/demo-pagamento/demo-pagamento.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-demo-pagamento',
   templateUrl: './demo-pagamento.component.html',
-  styleUrls: ['./demo-pagamento.component.css']
+  styleUrls: ['./demo-pagamento.component.css'],
 })
 export class DemoPagamentoComponent {
+  isEdit: boolean = false;
 
+  onToggleEdit() {
+    this.isEdit = !this.isEdit;
+  }
 }
